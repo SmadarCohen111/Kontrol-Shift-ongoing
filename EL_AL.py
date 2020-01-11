b33a55d10110fc1d7740f357a5a9f95da23ca1eb
smadar422 <49597158+smadar422@users.noreply.github.com>
2020-01-11 19:59:24 +0200

    change4

--- EL_AL.py
+++ EL_AL.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 
 class elal_emp:
     employee_name = " "
@@ -13,41 +11,67 @@ class elal_emp:
 
 
 p1 = elal_emp()
-check_arr = [["Smadar cohen", 58719, 2, True, True, False], ["Hagay tik", 28719, 3, True, True, True]]
+p3 = elal_emp()
+#pointer = 0
+check_arr = [["Smadar Cohen", 58719, 2, True, True, False], ["Hagay Tik", 28719, 3, True, True, True] , ["Osher Avraham", 59314, 1, True, False, False], ["Luba Epshtein", 59315, 4, True, False, True],
+             ["Adi Biton", 59317, 3, True, True, True], ["Amichai Friedman", 29136, 2.5, True, True, True], ["Amit Senouf", 29315, 4, True, True, False], ["Anna Kulik", 29317, 1, True, True, False],
+             ["Anna Zicherman", 38193, 4, True, False, False], ["Ayala Josef", 37195, 3, True, True, False], ["Bar Tangi", 37197, 1.5, True, False, False],["Bar Hanash", 43195, 3.5, True, True, False],
+             ["Chemdat Ofir Lipscker", 43193, 1, True, True, True], ["Chen Vashadi", 45193, 2, True, False, True], ["Einat Skornik", 65237, 3, True, False, False],["Eshchar Ben-Haiem", 67193, 2.5 , True, False, False],
+             ["Eylon Segal", 65233, 2, True, True, True], ["Gal Ben-nun", 57133, 2, True, True, False],["Gal Ben Yishay", 57135, 2, True, True, True]]
 
-
-def skills(check_arr):
+"""
+def emp_number(check_arr):
     for i in range(len(check_arr)):
         for j in range(6):
-          if type(check_arr[i][j]) == bool:
-           a = check_arr[i][j]
-           b = p1.skill.get("ground_attendant")
-           #p1.skill[j-3] = check_arr[i][j]
-           if a != b:
-               p1.skill["ground_attendant"] = a
-               print(p1.skill)
+            if type(check_arr[i][j]) == int and str(check_arr[i][j]).__len__() > 2:
+                if p1.employee_number == 0:
+                  p1.employee_number =  check_arr[i][j]
+                  print(p1.employee_number)
+                  break
+                elif p3.employee_number == 0 and p1.employee_number == 0:
+                  p3.employee_number = check_arr[i][j]
+                  print(p3.employee_number)
+                elif p1.employee_number != 0:
+                    break
+"""
 
 
-def name_employee(check_arr):
+def get_value(skill):
+    return list(p1.skill.values())
+
+def get_key(skill):
+    return list(p1.skill.keys())
+
+def skills(check_arr, counter):
     for i in range(len(check_arr)):
-       for j in range(len(check_arr)):
-        if(type(check_arr[i][j]) == str):
-           p1.employee_name = check_arr[i][j]
+        if i > counter:
+            counter += 1
+            name_employee(check_arr, counter)
+        for j in range(3,6):
+          if type(check_arr[counter][j]) == bool:
+           organ = check_arr[counter][j]
+           value = get_value(p1.skill)
+           if organ != value.pop(j-3):
+               key = get_key(p1.skill)
+               for i in range(1):
+                p1.skill[key.pop(j-3)] = organ
+        if i == counter:
+         print(p1.skill)
+
+
+def name_employee(check_arr, counter):
+     for i in range(len(check_arr)):
+        print("counter: ", counter, "length of arr: ", len(check_arr))
+        if type(check_arr[counter][i]) == str:
+          if i+1 != len(check_arr):
+           p1.employee_name = check_arr[counter][i]
            print(p1.employee_name, " is qualified to be: ")
-           skills(check_arr)
+           skills(check_arr, counter)
+          elif i+1 == len(check_arr):
+            break
+     return None
 
-def add_emp(check_arr):
-    for i in range(len(check_arr)):
 
-"""
-def print_now():
-    emp5 = elal_emp()
-    for x,y in emp5.skill.items():
-        print(x, y)
-    z = emp5.skill.get("s_s")
-    print(z)
-"""
 
-#print_now()
 p2 = elal_emp()
-name_employee(check_arr)
+name_employee(check_arr, 0)
